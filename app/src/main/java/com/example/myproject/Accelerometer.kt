7bce4d25c1af191c273a074c11ebc0984910034f
EldarOru <macdog108@gmail.com>
2021-04-15 19:59:49 +0300

    Refactor

--- app/src/main/java/com/example/myproject/Accelerometer.kt
+++ app/src/main/java/com/example/myproject/Accelerometer.kt
@@ -8,15 +8,16 @@ import android.hardware.SensorManager
 import kotlin.properties.Delegates
 
 class Accelerometer(context: Context): SensorEventListener{
-
+    var x: Float = 0.0f
 
     var sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     var sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
     override fun onSensorChanged(sensorEvent: SensorEvent?) {
+        x = sensorEvent!!.values[0]
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
+
     }
     fun register(){
         sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_FASTEST)
--- app/src/main/java/com/example/myproject/HomeActivity.kt
+++ app/src/main/java/com/example/myproject/HomeActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myproject
 
 import android.content.Context
+import android.content.Intent.getIntent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
@@ -18,8 +22,13 @@ class HomeActivity : AppCompatActivity(), SensorEventListener {
     lateinit var user: HashMap<String, Any>
     lateinit var userNameText: TextView
     lateinit var accelerometerTextView: TextView
+    lateinit var playSoundButton: Button
+    //lateinit var yameteSound: MediaPlayer
+    var yameteSound: MediaPlayer? = null
     lateinit var sensorManager: SensorManager
+    lateinit var audioManager: AudioManager
     //lateinit var accelerometer: Accelerometer
+
     var database = FirebaseDatabase.getInstance().getReference()
     override fun onSensorChanged(sensor: SensorEvent?) {
         accelerometerTextView.setText(sensor!!.values[0].toString())
@@ -32,12 +41,16 @@ class HomeActivity : AppCompatActivity(), SensorEventListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        playSoundButton = findViewById(R.id.playSoundButton)
         userNameText = findViewById(R.id.nameTextView)
         var intent = getIntent()
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 10,0)
         accelerometerTextView = findViewById(R.id.accelerometerTextView)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
         //accelerometer = Accelerometer(this)
+
         val menuListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 user = snapshot.getValue() as HashMap<String, Any>
@@ -47,7 +60,22 @@ class HomeActivity : AppCompatActivity(), SensorEventListener {
 
             }
         }
+
         database.child("users").child(intent.getStringExtra("userID")).addValueEventListener(menuListener)
 
+        playSoundButton.setOnClickListener {
+            if (yameteSound == null) {
+                yameteSound = MediaPlayer.create(this, R.raw.yamete)
+                yameteSound?.start()
+            }else{
+                yameteSound?.release()
+                yameteSound = null
+            }
+
+
+
+
+
+        }
         }
     }
